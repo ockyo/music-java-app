e96b649cc00e940ab0b6659dc7aa9538e959db6e
Ockyo <Ockyo.0605@gmail.com>
2024-08-22 23:51:19 +0700

    create a new frame

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -1,19 +1,67 @@
 package View;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class MainFrame extends JFrame{
-    private ProfilePanel profile;
-    public MainFrame(){
-        super("Profile");
-        setSize(600,700);
+import Model.ButtonCustom;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame {
+    private ButtonCustom addProfileBtn;
+    private JPanel panelContainer;
+
+    public MainFrame() {
+        super("Music App");
+        setLayout(new BorderLayout());
+        setSize(600, 700);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        profile = new ProfilePanel();
-        add(profile);
-        
+
+        addProfileBtn = new ButtonCustom("Them Profile", "src\\Image\\add.png");
+        addProfileBtn.setFont(new Font("Arial", Font.BOLD, 20));
+        add(addProfileBtn, BorderLayout.NORTH);
+
+        JButton addButton = new JButton("+");
+        addButton.setFont(new Font("Arial", Font.BOLD, 20));
+
+        // Tạo một JPanel để chứa các panel con
+        panelContainer = new JPanel();
+        panelContainer.setLayout(new GridLayout(0, 2, 10, 10));
+        panelContainer.setBackground(Color.BLUE);
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addNewPanel();
+            }
+        });
+
+        add(addButton, BorderLayout.NORTH);
+        // add(new JScrollPane(panelContainer), BorderLayout.CENTER);
+        add(panelContainer, BorderLayout.CENTER);
     }
+
+    private void addNewPanel() {
+        ProfilePanel profilePanel = new ProfilePanel();
+
+        profilePanel.setSize(50, 30);
+    
+        profilePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        // Thêm panel mới vào panelContainer
+        panelContainer.add(profilePanel);
+        // Cập nhật hiển thị
+        panelContainer.revalidate();
+        panelContainer.repaint();
+    }
+
     public static void main(String[] args) {
-        new MainFrame().setVisible(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
     }
 }
